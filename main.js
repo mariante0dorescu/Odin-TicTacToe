10e1211e38756558a532f3fed76cba4accefa93a
mariante0dorescu <broinkus@gmail.com>
2023-06-19 17:23:32 +0300

    needs fix computer move

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const setUserName = (e) => {
     form.action = "#game";
     
     playBtn.addEventListener("click", game);
-    return;
+    //return;
   }
 }
 
@@ -28,7 +28,7 @@ const game = () => {
   
   playerScreen.parentNode.removeChild(playerScreen);
 
-    const cells = document.querySelectorAll('.cell');
+  const cells = document.querySelectorAll('.cell');
   const resetButton = document.getElementById('reset');
   const modal = document.getElementById('modal');
   const closeModal = document.getElementById('close__modal');
@@ -41,7 +41,7 @@ const game = () => {
 
   // game variables
   const playerPoints = 0;
-  const computerPoints = 50;
+  const computerPoints = 0;
   let board = ["","","","","","","","",""];
   const winCombos = [[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8]];
   const playerMark = `<svg class="symbol x"><use href="#x"></use></svg>`;
@@ -50,13 +50,19 @@ const game = () => {
   let gameOver = false;
 
     // show player name and points / computer points
-    player.innerHTML = `<h2>${playerName}</h2>`
+  player.innerHTML = `<h2>${playerName}</h2>`
+ 
+  // udpdate the score
+  const updateScore = () => {
     playerScore.innerHTML = `<h2>${playerPoints}</h2>`
     computerScore.innerHTML = `<h2>${computerPoints}</h2>`
+  }
   
- 
-    // this function clear the cells and reset the game
+  updateScore();
+
+    // clear the cells and reset the game
   const resetGame = () => {
+    console.log('reset')
     gameOver = true;
     // reset the player one as the first player
     isPlayerOTurn = false
@@ -70,7 +76,111 @@ const game = () => {
 
     // event listener for reset button
   resetButton.addEventListener('click', resetGame);
+
+
+  // this functions alternates the inner html for the cell
+  // const currentPlayer = () => {
+  //   const currentPlayer = isPlayerOTurn ? computerMarkMark : playerMark;
+  //   isPlayerOTurn = !isPlayerOTurn;
+  //   return currentPlayer;
+  // }
+
+
+  //  shows the modal with a custom message
+  const showModal = (message) => {
+    let div = document.createElement('div');
+    div.classList.add('modal__message');
+    div.innerText = message;
+    modal.insertAdjacentElement("afterbegin", div)
+    modal.showModal();
+
+    closeModal.addEventListener('click', () => {
+      div.innerText = "";
+      modal.close();
+      resetGame();
+    })
+  }
+  // check if there is an empty slot in the board
+  const checkSlot = (id) => {
+  return board[id] === "";
+  }
+
+    // this functions checks is the board array is not c
+  const checkDraw = () => {
+    return !board.some((mark) => mark === "")
+  }
+
+  // check if there is a win combination or draw
+  const checkWin = (player) => {
+    if(checkDraw()){
+      gameOver = true;
+      showModal('draw')
+      return;
+    };
+  }
+
+  const randomComputerMove = () => {
+    const emptySlot = [];
+    for(let i = 0; i < board.length; i++) {
+      if(board[i] === ""){
+        emptySlot.push(i)
+      }
+    }
+
+    if(emptySlot.length > 0){
+      const randomMove = Math.floor(Math.random() * emptySlot.length);
+      return emptySlot[randomMove];
+    }
+
+    return -1;
+  }
+
+  const computerMove = () => {
+    board[randomComputerMove()] = "o";
+    console.log(cells.target)
+  }
+
+
+  // this functions add event listener to every cell and 
+  // inserts the current player into the clicked cell
+  // and tracks the moves by inserting the corresponding mark into board array
+  const clickCell = (e) => {
+    // select cell
+    const cell = e.target;
+  
+    //switch players to fill the board with moves
+   // let player = isPlayerOTurn ? console.log(computerMove()) : "x";
+
+
+    
+    // check if game is over
+    if(gameOver) {
+      //console.log('game over');
+      return;
+    } else {
+      if(checkSlot(cell.id)) {
+      cell.innerHTML = playerMark;
+      board[cell.id] = "x";
+      } 
+    }
+    console.log(computerMove())
+    checkWin(player);
+  }
+
+  const play = () => {
+    if(gameOver === false){
+      cells.forEach((cell) => {
+        cell.addEventListener('click', clickCell)
+      })
+    } else return;   
+  }
+
+  play()
+
 }
+
+
+
 
 
 
